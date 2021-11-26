fa645a6c784899f76996b93aca0f5ab9dbdaa9cb
Alex Olson <alexkellenolson@gmail.com>
2021-11-26 20:18:25 +0100

    test for noop refactord (#9)

--- extension/advanzia-assistant.spec.ts
+++ extension/advanzia-assistant.spec.ts
@@ -1,12 +1,25 @@
-import './advanzia-assistant';
 import { Script } from './advanzia-assistant';
 
 describe('Content Script', () => {
     it('should not do anything if not on transactions page', async () => {
-        await new Promise((resolve) => {
-            new Script()
-                .on('noop', () => resolve(void 0))
-                .execute();
-        });
+        const eventsRaised: { [key: string]: boolean } = {
+            noop: false,
+            ready: false,
+            done: false,
+            error: false,
+        };
+
+        const setRaised = (eventName: string) => () => { eventsRaised[eventName] = true };
+
+        await new Script()
+            .on('noop', setRaised('noop'))
+            .on('ready', () => setRaised('ready'))
+            .on('done', () => setRaised('done'))
+            .execute();
+
+        expect(eventsRaised.noop).toBe(true);
+        expect(eventsRaised.ready).toBe(false);
+        expect(eventsRaised.done).toBe(false);
+        expect(eventsRaised.error).toBe(false);
     });
 });
