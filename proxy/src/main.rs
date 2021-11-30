829b888193ea2fd66ebd379719424eddd032a25d
Alex Olson <alexkellenolson@gmail.com>
2021-11-30 01:45:35 +0100

    add logging example from hudsucker to proxy (wip) (#8)

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+use hudsucker::{
+    async_trait::async_trait,
+    certificate_authority::RcgenAuthority,
+    hyper::{Body, Request, Response},
+    *,
+};
+
+use log::*;
+use rustls_pemfile as pemfile;
+use std::net::SocketAddr;
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Failed to install CTRL+C signal handler");
+}
+
+#[derive(Clone)]
+struct LogHandler {}
+
+#[async_trait]
+impl HttpHandler for LogHandler {
+    async fn handle_request(
+        &mut self,
+        _ctx: &HttpContext,
+        req: Request<Body>,
+    ) -> RequestOrResponse {
+        println!("{:?}", req);
+        RequestOrResponse::Request(req)
+    }
+
+    async fn handle_response(&mut self, _ctx: &HttpContext, res: Response<Body>) -> Response<Body> {
+        println!("{:?}", res);
+        res
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let mut private_key_bytes: &[u8] = include_bytes!("../key.pem");
+    let mut ca_cert_bytes: &[u8] = include_bytes!("../cert.pem");
+    let private_key = rustls::PrivateKey(
+        pemfile::pkcs8_private_keys(&mut private_key_bytes)
+            .expect("Failed to parse private key")
+            .remove(0),
+    );
+    let ca_cert = rustls::Certificate(
+        pemfile::certs(&mut ca_cert_bytes)
+            .expect("Failed to parse CA certificate")
+            .remove(0),
+    );
+
+    let ca = RcgenAuthority::new(private_key, ca_cert, 1_000)
+        .expect("Failed to create Certificate Authority");
+    let proxy = ProxyBuilder::new()
+        .with_addr(SocketAddr::from(([127, 0, 0, 1], 3000)))
+        .with_rustls_client()
+        .with_ca(ca)
+        .with_http_handler(LogHandler {})
+        .build();
+
+    if let Err(e) = proxy.start(shutdown_signal()).await {
+        error!("{}", e);
+    }
 }
